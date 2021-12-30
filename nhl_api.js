914b993efd24ee5f5bcd50e8cff9175074e93ca7
jrmart12 <rodolfojose@live.com>
2021-12-30 09:12:56 -0600

    first commit

---
+++ nhl_api.js
@@ -0,0 +1,33 @@
+//DATE PICKER
+$(document).ready(function () {
+  $("#resetButton").click(function () {
+    $(".card-deck").empty();
+  });
+  $("#submit").click(function () {
+    var dateValue = $("#dateSelect").val();
+    $nhlLink = "https://www.nhl.com/gamecenter/ + $dateValue";
+
+    fetch("https://statsapi.web.nhl.com/api/v1/schedule?date=" + dateValue)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // $("#schedule-container").text(data.copyright);
+        data.dates.forEach((date) => {
+          date.games.forEach((game) => {
+            console.log(game);
+            console.count(game); //determined the number of games
+            let cardCount = 0;
+            $("#dateHeader").append("Games for " + dateValue);
+            $("#schedule-container").append("<br/>");
+            $("#schedule-container").append("<br/>");
+            $("#schedule-container").append(game.teams.away.team.name);
+            $("#schedule-container").append(" <strong> vs </strong>");
+            $("#schedule-container").append(game.teams.home.team.name);
+            $("#schedule-container").append("<br/>");
+            cardCount++;
+          });
+        });
+      });
+  });
+});
---
+++ test_nhl_api.js
@@ -0,0 +1,41 @@
+//DATE PICKER
+$(document).ready(function () {
+  $("#resetButton").click(function () {
+    $(".card").remove();
+  });
+  $("#submit").click(function () {
+    var dateValue = $("#dateSelect").val();
+    $nhlLink = "https://www.nhl.com/gamecenter/ + $dateValue";
+
+    fetch("https://statsapi.web.nhl.com/api/v1/schedule?date=" + dateValue)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // $("#schedule-container").text(data.copyright);
+        let cardCount = data.totalGames;
+        let x = 0;
+        while (x < cardCount) {
+          data.dates.forEach((date) => {
+            date.games.forEach((game) => {
+              console.log(game);
+              // $("#dateHeader").append(dateValue);
+              // $("#schedule-container" ).append("<br/>");
+              // $("#schedule-container" ).append("<br/>");
+              $("#schedule-container").append(`
+              <div class="card" style="width: auto;"> 
+                <div class="card-body">
+                  <h5 class="card-title">${game.teams.away.team.name} VS ${
+                game.teams.home.team.name
+              }</h5>
+                  <p class="card-text">Date: ${new Date(game.gameDate)}</p>
+                  <p class="card-text">Stadium: ${game.venue.name}</p>
+                </div>
+              </div>`);
+              x++;
+            });
+          });
+        }
+      });
+  });
+});
